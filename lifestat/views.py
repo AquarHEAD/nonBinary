ef9c700325128c2a00f836bceb3c8e09ad5a98c5
AquarHEAD <AquarHEAD@gmail.com>
2010-09-18 01:47:36 +0800

    Paginator added. Having some CSS problems.

--- lifestat/views.py
+++ lifestat/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render_to_response
 from datetime import datetime
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from nonBinary.lifestat.models import Stat, StatForm
 
 def home(request):
-    latest_stats = Stat.objects.all().order_by('-stat_date')
+    stats = Stat.objects.all().order_by('-stat_date')
     form = StatForm()
+    paginator = Paginator(stats, 13)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    
+    try:
+        latest_stats = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        latest_stats = paginator.page(paginator.num_pages)
     return render_to_response('lifestat/home.html',{'latest_stats': latest_stats, 'new_stat_form': form},
                               context_instance=RequestContext(request))
 
